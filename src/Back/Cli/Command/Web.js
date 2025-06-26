e4f615ecbb949e86544bbaa7d19e5ea60207d9d8
Alex Gusev <alex@flancer64.com>
2025-06-26 10:09:07 +0300

    SVTR-319 Use TeqCMS as a web server

--- src/Back/Cli/Command/Web.js
+++ src/Back/Cli/Command/Web.js
@@ -12,21 +12,24 @@ export default class Fl32_Cms_Back_Cli_Command_Web {
      * @param {Fl32_Cms_Back_Web_Handler_Template} handTmpl
      * @param {Fl32_Web_Back_Server} server
      */
-    constructor({
-        'node:path': path,
-        Fl32_Cms_Back_Config$: config,
-        Fl32_Web_Back_Dispatcher$: dispatcher,
-        Fl32_Web_Back_Handler_Pre_Log$: handLog,
-        Fl32_Web_Back_Handler_Static$: handStatic,
-        Fl32_Cms_Back_Web_Handler_Template$: handTmpl,
-        Fl32_Web_Back_Server$: server,
-    }) {
+    constructor(
+        {
+            'node:path': path,
+            Fl32_Cms_Back_Config$: config,
+            Fl32_Web_Back_Dispatcher$: dispatcher,
+            Fl32_Web_Back_Handler_Pre_Log$: handLog,
+            Fl32_Web_Back_Handler_Static$: handStatic,
+            Fl32_Cms_Back_Web_Handler_Template$: handTmpl,
+            Fl32_Web_Back_Server$: server,
+        }
+    ) {
         /* eslint-enable jsdoc/require-param-description,jsdoc/check-param-names */
         this.exec = async function () {
             const rootCms = config.getRootPath();
             const rootWeb = path.join(rootCms, 'web');
 
-            await handStatic.init({rootPath: rootWeb});
+            // TODO: configure statics after the refactoring
+            // await handStatic.init({rootPath: rootWeb});
 
             dispatcher.addHandler(handLog);
             dispatcher.addHandler(handStatic);
